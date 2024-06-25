c26d595b7277d5581f220c7a5989836a05ddcc0f
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-06-25 20:10:40 +0300

    ExceptionHandlerMiddleware added.

---
+++ PaparaDotnetBootcampApi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace PaparaDotnetBootcampApi.Middlewares
+{
+
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            if (exception is ArgumentException) code = HttpStatusCode.BadRequest;
+            else if (exception is KeyNotFoundException) code = HttpStatusCode.NotFound;
+
+            var result = JsonConvert.SerializeObject(new { status = (int)code, message = exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            return context.Response.WriteAsync(result);
+        }
+
+    }
+}
